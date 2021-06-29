704ab958d79e39572997c2e7f5e2857e34b313f1
youpinyao <497400448@qq.com>
2021-06-29 15:42:33 +0800

    1

--- src/getGitTagAndMessage.js
+++ src/getGitTagAndMessage.js
@@ -4,7 +4,7 @@ module.exports = () => {
   const getString = (output) => output.stdout.toString().trim();
   const tag = spawn.sync('git', ['describe', '--abbrev=0', '--tags']);
   const msg = spawn.sync('git', ['show', getString(tag)]);
-  const message = getString(msg).split('\n').splice(0, 5).join('\n');
+  const message = getString(msg).split('\n').splice(0, 5).join(' ');
 
   return {
     tag: getString(tag).replace(/^v/g, ''),

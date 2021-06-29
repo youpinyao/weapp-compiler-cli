c1da0fcbd844c875f6c791070350583bf0884bfb
youpinyao <497400448@qq.com>
2021-06-29 13:40:30 +0800

    1

---
+++ init.js
@@ -0,0 +1,24 @@
+const fs = require('fs-extra');
+const path = require('path');
+const packagePath = path.resolve(process.cwd(), 'package.json');
+
+if (fs.existsSync(packagePath)) {
+  const package = fs.readJSONSync(packagePath);
+  let hasChange = false;
+
+  if (!package.scripts) {
+    package.scripts = {};
+  }
+  if (!package.scripts['weapp-cli:upload']) {
+    package.scripts['weapp-cli:upload'] = 'weapp-cli upload';
+    hasChange = true;
+  }
+  if (!package.scripts['weapp-cli:preview']) {
+    package.scripts['weapp-cli:preview'] = 'weapp-cli preview';
+    hasChange = true;
+  }
+
+  if (hasChange) {
+    fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
+  }
+}

fce5c60ea71c628b2cf3b522d71f58e6813213b0
youpinyao <497400448@qq.com>
2021-11-08 16:13:36 +0800

    fix: git 获取内容优化

--- src/getGitLastTagAndMessage.js
+++ src/getGitLastTagAndMessage.js
@@ -1,9 +1,9 @@
 const spawn = require('cross-spawn');
 
 module.exports = () => {
-  const getString = (output) => (output.stdout || output.stderr || 'undefined').toString().trim();
-  const tag = spawn.sync('git', ['describe', '--abbrev=0', '--tags']);
-  const msg = spawn.sync('git', ['show', '-q', getString(tag)]);
+  const getString = (output) => (output.stdout || output.stderr || '').toString().trim();
+  const tag = getString(spawn.sync('git', ['describe', '--abbrev=0', '--tags'])) || '0.0.0';
+  const msg = spawn.sync('git', ['show', '-q', tag]);
   const message = getString(msg)
     .split('\n')
     .filter((item) => !!item)
@@ -11,7 +11,7 @@ module.exports = () => {
     .join(' ');
 
   return {
-    tag: getString(tag).replace(/^v/g, ''),
+    tag: tag.replace(/^v/g, ''),
     message,
   };
 };

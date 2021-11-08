b13ce6159b0781e0b20c197756e3a187a222e4ce
youpinyao <497400448@qq.com>
2021-11-08 16:42:33 +0800

    fix: git 获取内容优化

--- src/getEnv.js
+++ src/getEnv.js
@@ -3,15 +3,16 @@ const fs = require('fs-extra');
 const { projectPath } = require('./config');
 const ENV = require('./ENV');
 const envPath = path.join(projectPath, '.weapp');
-let env = {};
+let env = {
+  env: ENV.UNKOWN,
+};
 
 if (fs.pathExistsSync(envPath)) {
   try {
-    env = fs.readJSONSync(envPath) || {
-      env: ENV.UNKOWN,
-    };
+    env = fs.readJSONSync(envPath)
   } catch (error) {
-    // console.log(error);
+    console.log(envPath);
+    console.log(error);
   }
 }
 

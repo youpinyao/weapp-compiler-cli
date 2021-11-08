aa832df37e81273635f2ff7cc6b1c3059fc94d79
youpinyao <497400448@qq.com>
2021-11-08 17:49:36 +0800

    feat: get Env

--- src/getEnv.js
+++ src/getEnv.js
@@ -2,12 +2,10 @@ const path = require('path');
 const fs = require('fs-extra');
 const { projectPath } = require('./config');
 const ENV = require('./ENV');
-const envPath = path.join(projectPath, '.weapp');
+const envPath = path.join(projectPath, 'weapp.env.json');
 let env = {
   env: ENV.UNKOWN,
 };
-
-console.log('envPath', envPath, fs.existsSync(envPath), fs.pathExistsSync(envPath));
 
 if (fs.pathExistsSync(envPath)) {
   try {

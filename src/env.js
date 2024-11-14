c8ccaeb0451e6d2be8e42a496f7b6a0d84e4e260
youpinyao <youpinyao@anta.com>
2024-11-14 15:44:23 +0800

    feat: 添加自定义env

--- src/env.js
+++ src/env.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const fs = require('fs-extra');
+const yaml = require('yaml');
+const fse = require('fs-extra');
+const weappEnv = path.resolve(process.cwd(), '.weapp.env');
 const { getConfig } = require('./config');
 const ENV = {
   DEV: 'development',
@@ -7,6 +9,12 @@ const ENV = {
   SIMULATION: 'simulation',
   UNKNOWN: 'unknown',
 };
+
+if (fse.existsSync(weappEnv)) {
+  Object.assign(ENV, yaml.parse(fse.readFileSync(weappEnv).toString()));
+}
+
+
 let env = {
   env: ENV.UNKNOWN,
 };
@@ -15,14 +23,14 @@ module.exports = {
   ENV,
   getEnv: () => {
     const envPath = path.join(getConfig().projectPath, 'weapp.env.json');
-    if (fs.pathExistsSync(envPath)) {
+    if (fse.pathExistsSync(envPath)) {
       try {
-        env = fs.readJSONSync(envPath);
+        env = fse.readJSONSync(envPath);
       } catch (error) {
         console.log(error);
       }
     } else {
-      throw new Error(`${envPath} 不能存在，结构为 {"env": "development" | "production" | "simulation"}`)
+      throw new Error(`${envPath} 不存在，结构为 {"env": "development" | "production" | "simulation" | 自定义}`)
     }
     return env;
   },
